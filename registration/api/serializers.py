7b945c27c583b54bd15fb28a451be2088be64e4a
Javier <javierpinya@protonmail.com>
2023-04-28 11:24:50 +0200

    added endpoint register

--- registration/api/serializers.py
+++ registration/api/serializers.py
@@ -5,4 +5,14 @@ from registration.models import User
 class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "email", "first_name", "password"]
+        fields = ["id", "email", "username", "password"]
+
+    def create(self, validated_date):
+        password = validated_date.pop("password", None)
+        instance = self.Meta.model(**validated_date)
+
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
