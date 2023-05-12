35f59e5af286573b78108dc8bb6b3c0bcad63c84
Javier <javierpinya@protonmail.com>
2023-05-12 14:22:28 +0200

    BACKEND-10/crear app videos

---
+++ video/models.py
@@ -0,0 +1,25 @@
+import os
+from django.db import models
+from django.contrib.auth.models import User
+
+
+def upload_video_path(instance, filename):
+    old_instance = Video.objects.get(pk=instance.pk)
+    old_instance.video.delete()
+    return "video/" + filename
+
+
+def upload_image_path(instance, filename):
+    old_instance = Video.objects.get(pk=instance.pk)
+    old_instance.video.delete()
+    return "image_video/" + filename
+
+
+class Video(models.Model):
+    description = models.TextField()
+    video = models.FileField(upload_to=upload_video_path)
+    image = models.ImageField(upload_to=upload_image_path)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shared_counter = models.IntegerField(default=0)
+    likes_counter = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
