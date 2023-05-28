3689d5e406ff8e1a5c48a8d8c029a3648da315da
Javier <javierpinya@protonmail.com>
2023-05-28 09:04:05 +0200

    dev token time

--- baby_backend/settings.py
+++ baby_backend/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=99),
     "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=7),
 }
 
